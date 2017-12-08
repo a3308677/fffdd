06c3f0bbef21002689329eb2794a600af201a8dc
5414 <33489932+a3308677@users.noreply.github.com>
2017-12-08 16:28:52 +0900

    Update app3.py

--- app3.py
+++ app3.py
@@ -207,25 +207,7 @@ def googlei(query,n):
     random.shuffle(x)    
     return x
 
-def main():
-    """Shows basic usage of the Google Drive API.
 
-    Creates a Google Drive API service object and outputs the names and IDs
-    for up to 10 files.
-    """
-    credentials = get_credentials()
-    http = credentials.authorize(httplib2.Http())
-    service = discovery.build('drive', 'v3', http=http)
-
-    results = service.files().list(
-        pageSize=10,fields="nextPageToken, files(id, name)").execute()
-    items = results.get('files', [])
-    if not items:
-        print('No files found.')
-    else:
-        print('Files:')
-        for item in items:
-            return('{0} ({1})'.format(item['name'], item['id']))
             
 
 @handler.add(MessageEvent, message=TextMessage)
