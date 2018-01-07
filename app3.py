5e8f06eb324b1a536b717412366c7321be9df3a8
5414 <33489932+a3308677@users.noreply.github.com>
2018-01-07 19:02:54 +0900

    Update app3.py

--- app3.py
+++ app3.py
@@ -621,7 +621,16 @@ def handle_message(event):
         imagemap_message = ImagemapSendMessage(
             base_url='https://i.ytimg.com/vi/SqfdJ5DGusQ/hqdefault.jpg?sqp=-oaymwEXCPYBEIoBSFryq4qpAwkIARUAAIhCGAE=&amp;rs=AOn4CLBTwHpPcPpBTUq0Wl2u4cZWsMUseA',
             alt_text='this is an imagemap',
-            base_size=BaseSize(height=1040, width=1040),
+            base_size=BaseSize(x=0, y=0,height=520, width=520),
+            base_url='https://i.ytimg.com/vi/SqfdJ5DGusQ/hqdefault.jpg?sqp=-oaymwEXCPYBEIoBSFryq4qpAwkIARUAAIhCGAE=&amp;rs=AOn4CLBTwHpPcPpBTUq0Wl2u4cZWsMUseA',
+            alt_text='this is an imagemap',
+            base_size=BaseSize(x=530, y=0,height=510, width=520),
+            base_url='https://i.ytimg.com/vi/SqfdJ5DGusQ/hqdefault.jpg?sqp=-oaymwEXCPYBEIoBSFryq4qpAwkIARUAAIhCGAE=&amp;rs=AOn4CLBTwHpPcPpBTUq0Wl2u4cZWsMUseA',
+            alt_text='this is an imagemap',
+            base_size=BaseSize(x=0, y=520,height=510, width=510),
+            base_url='https://i.ytimg.com/vi/SqfdJ5DGusQ/hqdefault.jpg?sqp=-oaymwEXCPYBEIoBSFryq4qpAwkIARUAAIhCGAE=&amp;rs=AOn4CLBTwHpPcPpBTUq0Wl2u4cZWsMUseA',
+            alt_text='this is an imagemap',
+            base_size=BaseSize(x=530, y=530,height=500, width=500),
             actions=[
                 URIImagemapAction(
                     link_uri='http://www.eyny.com/thread-10447098-1-1.html',
